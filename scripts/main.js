8b27f1908d90385c1fb1488afe7fded29e24c825
Patrick Marmaroli <pmarmaroli@microsoft.com>
2024-06-27 16:42:24 +0300

    testing a new template

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('linkedin/Profile.csv')
+    fetch('data/data.csv')
         .then(response => response.text())
         .then(data => {
-            const profile = parseCSV(data)[0];
-            document.getElementById('name').innerText = `${profile['First Name']} ${profile['Last Name']}`;
-            document.getElementById('headline').innerText = profile['Headline'];
-            document.getElementById('summary-text').innerText = profile['Summary'];
-        });
-
-    fetch('linkedin/Education.csv')
-        .then(response => response.text())
-        .then(data => {
-            const education = parseCSV(data);
-            const educationContent = document.getElementById('education-content');
-            education.forEach(entry => {
+            const items = parseCSV(data);
+            const content = document.getElementById('content');
+            items.forEach(item => {
                 const div = document.createElement('div');
+                div.className = 'box';
                 div.innerHTML = `
-                    <h4>${entry['School Name']}</h4>
-                    <p>${entry['Degree Name']}, ${entry['Start Date']} - ${entry['End Date']}</p>
-                    <p>Notes: ${entry['Notes'] || 'N/A'}</p>
-                    <p>Activities: ${entry['Activities'] || 'N/A'}</p>
+                    <h3>${item['Title']}</h3>
+                    <a href="${item['URL']}" target="_blank">${item['URL']}</a>
+                    <p>${item['Description']}</p>
                 `;
-                educationContent.appendChild(div);
-            });
-        });
-
-    fetch('linkedin/Positions.csv')
-        .then(response => response.text())
-        .then(data => {
-            const positions = parseCSV(data);
-            const positionsContent = document.getElementById('positions-content');
-            positions.forEach(entry => {
-                const div = document.createElement('div');
-                div.innerHTML = `
-                    <h4>${entry['Company Name']}</h4>
-                    <p>${entry['Title']}, ${entry['Started On']} - ${entry['Finished On'] || 'Present'}</p>
-                    <p>${entry['Description']}</p>
-                `;
-                positionsContent.appendChild(div);
+                content.appendChild(div);
             });
         });
 
